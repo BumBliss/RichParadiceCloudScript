53cab2852463fc9c0954779714690207c3aceacf
Lertluck Leela-amornsin <lertlove@hotmail.com>
2016-04-07 14:08:40 +0700

    welcome

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 handlers.helloWorld = function (args)
 {
-	var message = "Hello " + args.name;
-		log.info(message);
+	var message = "Welcome " + args.name;
+	log.info(message);
+
 	return { messageValue: message };
 }
 
